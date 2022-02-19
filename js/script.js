ca9485fe284ebf06057729e16e5f8d8275d6c7ef
Lazaro Queiroz <lazaroq11@hotmail.com>
2022-02-19 00:51:28 -0300

    feat: Modal de Exclusão

--- js/script.js
+++ js/script.js
@@ -1,15 +1,43 @@
 let btEdit = document.querySelector(".AskTips .editar")
+let btDel = document.querySelector(".AskTips .deletar")
+let btCanc = document.querySelector(".modal-container-del .btCanc")
+let btFecha = document.querySelector(".botaoFechar")
+let modalEdit = document.querySelector("#modal");
+let modalDel = document.querySelector("#modalDel")
 
-let modal = document.querySelector("#modal");
 let contador = 0;
  
 
     btEdit.addEventListener("click",()=>{
-        contador++;
+       
         if(modal.style.display == ""){
             modal.style.display = "flex"
         }else{
             modal.style.display = ""
         }
 
+    })
+    
+    btFecha.addEventListener("click",()=>{
+        if(modal.style.display == ""){
+            modal.style.display = "flex"
+        }else{
+            modal.style.display = ""
+        }
+    })
+
+    btDel.addEventListener("click",()=>{
+       if(modalDel.style.display==""){
+           modalDel.style.display = "flex"
+       }else{
+           modalDel.style.display = ""
+       }
+    })
+
+    btCanc.addEventListener("click",()=>{
+        if(modalDel.style.display==""){
+            modalDel.style.display = "flex"
+        }else{
+            modalDel.style.display = ""
+        }
     })
